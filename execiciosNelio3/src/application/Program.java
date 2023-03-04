d0353c749cc9dbd32cb3c2609228f6275c89c779
cristovanlopes <cristovanl1@gmail.com>
2023-03-03 23:51:59 -0300

    EXERCICIO 3 NELIO ALVES CONCLUIDO, O ALGORITIMO DEVE RETORNAR SE O ALUNO PASSOU SE NÃO RETORNAR QUANTO FALTA DE NOTA

--- execiciosNelio3/src/application/Program.java
+++ execiciosNelio3/src/application/Program.java
@@ -1,7 +1,25 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Student;
+
 public class Program {
 	public static void main(String[] args) {
 		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		Student student = new Student();
+		
+		
+		student.name = sc.nextLine();
+		student.note1 = sc.nextDouble();
+		student.note2 = sc.nextDouble();
+		student.note3 = sc.nextDouble();
+		
+		System.out.println("FINAL GRADE = " + student.passFailed());
+		
+		sc.close();
 	}
 }
--- execiciosNelio3/src/entities/Student.java
+++ execiciosNelio3/src/entities/Student.java
@@ -6,14 +6,19 @@ public class Student {
 	public double note2;
 	public double note3;
 	
+	
 	public  double noteEnd() {
 		return (this.note1* 30 + this.note2 * 35 + this.note3 * 35) / 100;
 		
 	}
 	public String passFailed() {
 		if(noteEnd() > 60.00) {
-			return "GRADE" + noteEnd() + "PASS";
-		}else if()
+			return noteEnd() + "\n"+ "PASS";
+		}
+		else {
+			return noteEnd() + "\n" + "FAILED" + "\n" + "MISSING " + (60.00 - noteEnd() + " POINTS");
+		}
+		
 		
 	}
 }
