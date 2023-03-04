254595c12ec94e9d0219b217198d71560d7c6a07
cristovanlopes <cristovanl1@gmail.com>
2023-03-03 23:34:29 -0300

    Adcionado metodos e Objetos

---
+++ execiciosNelio3/src/entities/Student.java
@@ -0,0 +1,19 @@
+package entities;
+
+public class Student {
+	public String name;
+	public double note1;
+	public double note2;
+	public double note3;
+	
+	public  double noteEnd() {
+		return (this.note1* 30 + this.note2 * 35 + this.note3 * 35) / 100;
+		
+	}
+	public String passFailed() {
+		if(noteEnd() > 60.00) {
+			return "GRADE" + noteEnd() + "PASS";
+		}else if()
+		
+	}
+}
